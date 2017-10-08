f11d3c162341c71973ca933bfb0c914715e20322
Epulapp <Epulapp@epulapp109.univ-lyon1.fr>
2017-10-08 13:18:56 +0200

    évolution du menu en cours

--- src/com/polytech/Main.java
+++ src/com/polytech/Main.java
@@ -6,46 +6,39 @@ import java.util.Scanner;
 
 public class Main {
 	static List<Personnage> personnages = new ArrayList<Personnage>();
-    static List<ArmeLourde> armesLourdes = new ArrayList<ArmeLourde>();
-    static List<ArmeMagique> armesMagiques = new ArrayList<ArmeMagique>();
-    static List<ArmeDistance> armesDistances = new ArrayList<ArmeDistance>();
-    static List<Equipement> equipements = new ArrayList<Equipement>();
-	/*public static int afficherMenuPrincipal() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("********************************");
-		System.out.println("*************MENU***************");
-		System.out.println("********************************");
-		System.out.println("1. Règles du jeu");
-		System.out.println("2.Commencer le chargement de la partie");
-		System.out.println("3.Quitter");
-		System.out.print("Votre choix : ");
-		String choix = s.nextLine();
-		int chiffre = -1;
-		try {
-			chiffre = Integer.parseInt(choix);
-		} catch (Exception e) {
-			System.out.println("Le choix effectué n'est pas bon. veuillez réessayer");
-			afficherMenuPrincipal();
-		}
-		return chiffre;
+	static List<ArmeLourde> armesLourdes = new ArrayList<ArmeLourde>();
+	static List<ArmeMagique> armesMagiques = new ArrayList<ArmeMagique>();
+	static List<ArmeDistance> armesDistances = new ArrayList<ArmeDistance>();
+	static List<Equipement> equipements = new ArrayList<Equipement>();
+	static List<Personnage> perso_joueurs = new ArrayList<Personnage>();
+	static List<Integer> perso_choisi = new ArrayList<Integer>();
+	/*
+	 * public static int afficherMenuPrincipal() { Scanner s = new
+	 * Scanner(System.in);
+	 * System.out.println("********************************");
+	 * System.out.println("*************MENU***************");
+	 * System.out.println("********************************");
+	 * System.out.println("1. Règles du jeu");
+	 * System.out.println("2.Commencer le chargement de la partie");
+	 * System.out.println("3.Quitter"); System.out.print("Votre choix : ");
+	 * String choix = s.nextLine(); int chiffre = -1; try { chiffre =
+	 * Integer.parseInt(choix); } catch (Exception e) {
+	 * System.out.println("Le choix effectué n'est pas bon. veuillez réessayer"
+	 * ); afficherMenuPrincipal(); } return chiffre;
+	 * 
+	 * }
+	 */
 
-	}*/
-	
-	
-	/*public static int replay(){
-		Scanner s = new Scanner(System.in);
-		System.out.println("Voulez-vous rejouer? (Taper 1 pour oui 0 pour non) : ");
-		int choix_replay = -1;
-		try{
-			choix_replay = s.nextInt();
-		}catch(Exception e){
-			System.out.println("Veuillez saisir un choix valide!");
-			System.out.println("\n");
-			replay();
-		}
-		return choix_replay;
-		
-	}*/
+	/*
+	 * public static int replay(){ Scanner s = new Scanner(System.in);
+	 * System.out.
+	 * println("Voulez-vous rejouer? (Taper 1 pour oui 0 pour non) : "); int
+	 * choix_replay = -1; try{ choix_replay = s.nextInt(); }catch(Exception e){
+	 * System.out.println("Veuillez saisir un choix valide!");
+	 * System.out.println("\n"); replay(); } return choix_replay;
+	 * 
+	 * }
+	 */
 
 	public static void main(String[] args) {
 		boolean arret = false;
@@ -73,30 +66,56 @@ public class Main {
 				System.out.println("BON JEU!");
 				System.out.println("\n");
 				System.out.println("\n");
-				//test
-				/*creerPersonnages();
-				for(Equipement e : equipements){
-					System.out.println(e.getPointsAttaque());
-				}*/
+				// test
+				/*
+				 * creerPersonnages(); for(Equipement e : equipements){
+				 * System.out.println(e.getPointsAttaque()); }
+				 */
 				break;
 			case 2:
 				boolean quit = false;
 				boolean replay = false;
+				boolean control = false;
+				int cpt = 1;
+				int choix_perso = -1;
+				creerPersonnages();
 				System.out.println("Choisissez un personnage parmi la liste ci-dessous :");
-				/*fin du jeu possibilité de recommencer*/
-				do{
-					System.out.println("Voulez-vous recommencer? (o/n) : ");
-					String s = new Scanner(System.in).nextLine();
-					if(!s.equalsIgnoreCase("n") && !s.equalsIgnoreCase("o")){
-						quit = false;
-					}else if(s.equalsIgnoreCase("n")){
-						quit = true;
-						System.out.println("Vous quittez le jeu!");
-						System.exit(0);
-					}else if(s.equalsIgnoreCase("o")){
-						replay = true;
+				for (Personnage p : personnages) {
+					System.out.println(cpt + "-> " + "Nom : " + p.getNom() + "\n  Arme principale : "
+							+ p.getArmesPersonnages().get(1).getNomArme() + "\n  PV : " + p.getPointsVie() + "\n Tapez "
+							+ cpt + " pour le choisir");
+					cpt++;
+				}
+				do {
+					try {
+						System.out.print("Votre choix : ");
+						choix_perso = new Scanner(System.in).nextInt();
+						if (choix_perso >= 1 && choix_perso <= 6) {
+							if (perso_choisi.contains(choix_perso)) {
+								System.out.println("Ce personnage a déjà été choisi, veuillez en sélectionner un autre!");
+							} else {
+								perso_choisi.add(choix_perso);
+								System.out.println("Le joueur a choisi comme personnage : "  + personnages.get(choix_perso - 1).getNom());
+								control = true;
+							}
+						} else {
+							System.out.println("Mauvais choix! Veuillez effectuer un autre choix");
+						}
+
+					} catch (Exception e) {
+						System.out.println("Vous devez choisir un personnage entre 1 et 6");
 					}
-				}while(!replay && !quit );
+				} while (!control);
+				/* fin du jeu possibilité de recommencer */
+				/*
+				 * do{ System.out.println("Voulez-vous recommencer? (o/n) : ");
+				 * String s = new Scanner(System.in).nextLine();
+				 * if(!s.equalsIgnoreCase("n") && !s.equalsIgnoreCase("o")){
+				 * quit = false; }else if(s.equalsIgnoreCase("n")){ quit = true;
+				 * System.out.println("Vous quittez le jeu!"); System.exit(0);
+				 * }else if(s.equalsIgnoreCase("o")){ replay = true; }
+				 * }while(!replay && !quit );
+				 */
 				break;
 			case 3:
 				arret = true;
@@ -106,95 +125,91 @@ public class Main {
 			}
 		} while (!arret);
 	}
-	
+
 	public static void creerPersonnages() {
-	    // Création des Armes
-	       ArmeDistance arc = new ArmeDistance("Arc", 8, 10);
-	       ArmeDistance arbalete = new ArmeDistance("Arbalète", 6, 15);
-	       ArmeDistance couteaux = new ArmeDistance("Couteaux de lancer", 4, 5);
+		// Création des Armes
+		ArmeDistance arc = new ArmeDistance("Arc", 8, 10);
+		ArmeDistance arbalete = new ArmeDistance("Arbalète", 6, 15);
+		ArmeDistance couteaux = new ArmeDistance("Couteaux de lancer", 4, 5);
 
-	       ArmeLourde epee = new ArmeLourde("Épée", 3, 15);
-	       ArmeLourde hache = new ArmeLourde("Hache", 1, 15);
-	       ArmeLourde fleau = new ArmeLourde("Fleau", 2, 15);
-	       ArmeLourde dague = new ArmeLourde("Dague", 4, 4);
+		ArmeLourde epee = new ArmeLourde("Épée", 3, 15);
+		ArmeLourde hache = new ArmeLourde("Hache", 1, 15);
+		ArmeLourde fleau = new ArmeLourde("Fleau", 2, 15);
+		ArmeLourde dague = new ArmeLourde("Dague", 4, 4);
 
-	       ArmeMagique baton = new ArmeMagique("Bâton magique", 8, 10);
-	       ArmeMagique feu = new ArmeMagique("Boules de feu", 8, 10);
-	       ArmeMagique glace = new ArmeMagique("Boules de glace", 8, 10);
+		ArmeMagique baton = new ArmeMagique("Bâton magique", 8, 10);
+		ArmeMagique feu = new ArmeMagique("Boules de feu", 8, 10);
+		ArmeMagique glace = new ArmeMagique("Boules de glace", 8, 10);
 
+		// Création des Equipements
+		Equipement bouclier = new Equipement("Bouclier", 0, 6);
+		Equipement casque = new Equipement("Casque", 0, 5);
+		Equipement cheval = new Equipement("Cheval", 5, 1);
 
-	       // Création des Equipements
-	       Equipement bouclier = new Equipement("Bouclier", 0, 6);
-	       Equipement casque = new Equipement("Casque", 0, 5);
-	       Equipement cheval = new Equipement("Cheval", 5, 1);
+		// Création des Personnages
+		Mage gandalf = new Mage("Gandalf le Blanc", 100, 1, 1);
+		Mage sarouman = new Mage("Sarouman le Gris", 100, 1, 1);
 
+		Archer legolas = new Archer("Legolas", 100, 1, 1);
+		Archer tauriel = new Archer("Tauriel", 100, 1, 1);
 
-	       // Création des Personnages
-	       Mage gandalf = new Mage("Gandalf le Blanc", 100, 1, 1);
-	       Mage sarouman = new Mage("Sarouman le Gris", 100, 1, 1);
+		Guerrier bilbo = new Guerrier("Bilbo", 100, 1, 1);
+		Guerrier aragorn = new Guerrier("Aragorn", 100, 1, 1);
 
-	       Archer legolas = new Archer("Legolas", 100, 1, 1);
-	       Archer tauriel = new Archer("Tauriel", 100, 1, 1);
+		// On associe les armes et équipements aux personnages
+		gandalf.ajouterArme(baton);
+		gandalf.ajouterArme(feu);
+		gandalf.setEquipement(cheval);
 
-	       Guerrier bilbo = new Guerrier("Bilbo", 100, 1, 1);
-	       Guerrier aragorn = new Guerrier("Aragorn", 100, 1, 1);
-	       
-	       // On associe les armes et équipements aux personnages
-	       gandalf.ajouterArme(baton);
-	       gandalf.ajouterArme(feu);
-	       gandalf.setEquipement(cheval);
-	       
-	       sarouman.ajouterArme(baton);
-	       sarouman.ajouterArme(glace);
-	       sarouman.setEquipement(bouclier);
-	       
-	       legolas.ajouterArme(arc);
-	       legolas.ajouterArme(couteaux);
-	       legolas.setEquipement(bouclier);
-	       
-	       tauriel.ajouterArme(arc);
-	       tauriel.ajouterArme(arbalete);
-	       tauriel.setEquipement(cheval);
-	       
-	       bilbo.ajouterArme(hache);
-	       bilbo.ajouterArme(dague);
-	       bilbo.setEquipement(bouclier);
-	       
-	       aragorn.ajouterArme(epee);
-	       aragorn.ajouterArme(dague);
-	       aragorn.setEquipement(cheval);
-	       
-	       
-	       // Création des listes
-	       //List<Personnage> personnages = new ArrayList<Personnage>();
-	       personnages.add(gandalf);
-	       personnages.add(sarouman);
-	       personnages.add(legolas);
-	       personnages.add(tauriel);
-	       personnages.add(bilbo);
-	       personnages.add(aragorn);
+		sarouman.ajouterArme(baton);
+		sarouman.ajouterArme(glace);
+		sarouman.setEquipement(bouclier);
 
-	       //List<ArmeLourde> armesLourdes = new ArrayList<ArmeLourde>();
-	       armesLourdes.add(epee);
-	       armesLourdes.add(hache);
-	       armesLourdes.add(fleau);
-	       armesLourdes.add(dague);
+		legolas.ajouterArme(arc);
+		legolas.ajouterArme(couteaux);
+		legolas.setEquipement(bouclier);
 
-	       //List<ArmeMagique> armesMagiques = new ArrayList<ArmeMagique>();
-	       armesMagiques.add(baton);
-	       armesMagiques.add(feu);
-	       armesMagiques.add(glace);
+		tauriel.ajouterArme(arc);
+		tauriel.ajouterArme(arbalete);
+		tauriel.setEquipement(cheval);
 
-	       //List<ArmeDistance> armesDistances = new ArrayList<ArmeDistance>();
-	       armesDistances.add(arc);
-	       armesDistances.add(arbalete);
-	       armesDistances.add(couteaux);
+		bilbo.ajouterArme(hache);
+		bilbo.ajouterArme(dague);
+		bilbo.setEquipement(bouclier);
 
-	       //List<Equipement> equipements = new ArrayList<Equipement>();
-	       equipements.add(bouclier);
-	       equipements.add(casque);
-	       equipements.add(cheval);
+		aragorn.ajouterArme(epee);
+		aragorn.ajouterArme(dague);
+		aragorn.setEquipement(cheval);
+
+		// Création des listes
+		// List<Personnage> personnages = new ArrayList<Personnage>();
+		personnages.add(gandalf);
+		personnages.add(sarouman);
+		personnages.add(legolas);
+		personnages.add(tauriel);
+		personnages.add(bilbo);
+		personnages.add(aragorn);
+
+		// List<ArmeLourde> armesLourdes = new ArrayList<ArmeLourde>();
+		armesLourdes.add(epee);
+		armesLourdes.add(hache);
+		armesLourdes.add(fleau);
+		armesLourdes.add(dague);
+
+		// List<ArmeMagique> armesMagiques = new ArrayList<ArmeMagique>();
+		armesMagiques.add(baton);
+		armesMagiques.add(feu);
+		armesMagiques.add(glace);
+
+		// List<ArmeDistance> armesDistances = new ArrayList<ArmeDistance>();
+		armesDistances.add(arc);
+		armesDistances.add(arbalete);
+		armesDistances.add(couteaux);
+
+		// List<Equipement> equipements = new ArrayList<Equipement>();
+		equipements.add(bouclier);
+		equipements.add(casque);
+		equipements.add(cheval);
 	}
-
 
 }
